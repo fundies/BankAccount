ce2d0c002594db85c7e2a6a57094219244fbbbb5
fundies <cheeseboy16@gmail.com>
2015-05-30 20:11:12 +0000

    k

--- src/bankaccount/BankAccount.java
+++ src/bankaccount/BankAccount.java
@@ -7,65 +7,61 @@ package bankaccount;
 
 public class BankAccount {
 
-	/**
-	 * Balance variable.
-	 */
-	private double balance;
+    /**
+     * Balance variable.
+     */
+    private double balance;
 
-	/**
-	 * Constructs a bank account with a zero balance.
-	 */
-	public BankAccount() {
-		balance = 0;
-	}
+    /**
+     * Constructs a bank account with a zero balance.
+     */
+    public BankAccount() {
+        balance = 0;
+    }
 
-	/**
-	 * Constructs a bank account with a given balance.
-	 *
-	 * @param initialBalance
-	 *            the initial balance
-	 */
-	public BankAccount(final double initialBalance) {
-		balance = initialBalance;
-	}
+    /**
+     * Constructs a bank account with a given balance.
+     *
+     * @param initialBalance the initial balance
+     */
+    public BankAccount(final double initialBalance) {
+        balance = initialBalance;
+    }
 
-	/**
-	 * Deposits money into the bank account.
-	 *
-	 * @param amount
-	 *            the amount to deposit
-	 */
-	public final void deposit(final double amount) {
-		// double newBalance = balance + amount;
-		balance += amount;
-	}
+    /**
+     * Deposits money into the bank account.
+     *
+     * @param amount the amount to deposit
+     */
+    public final void deposit(final double amount) {
+        // double newBalance = balance + amount;
+        balance += amount;
+    }
 
-	/**
-	 * Withdraws money from the bank account.
-	 *
-	 * @param amount
-	 *            the amount to withdraw
-	 */
-	public final void withdraw(final double amount) {
-		balance -= amount + 1;
-	}
+    /**
+     * Withdraws money from the bank account.
+     *
+     * @param amount the amount to withdraw
+     */
+    public final void withdraw(final double amount) {
+        balance -= amount + 1;
+    }
 
-	/**
-	 * Gets the current balance of the bank account.
-	 *
-	 * @return the current balance
-	 */
-	public final double getBalance() {
-		return balance;
-	}
+    /**
+     * Gets the current balance of the bank account.
+     *
+     * @return the current balance
+     */
+    public final double getBalance() {
+        return balance;
+    }
 
-	/**
-	 * Sets the current balance of the bank account.
-	 *
-	 * @param amount
-	 *            set the balance to this amount
-	 */
-	public final void setBalance(final double amount) {
-		balance = amount;
-	}
+    /**
+     * Sets the current balance of the bank account.
+     *
+     * @param amount set the balance to this amount
+     */
+    public final void setBalance(final double amount) {
+        balance = amount;
+    }
 }
--- src/bankaccount/BankAccountTest.java
+++ src/bankaccount/BankAccountTest.java
@@ -9,56 +9,56 @@ import static org.junit.Assert.*;
  * @version 0.1
  */
 public class BankAccountTest {
-	/**
-	 * Test of deposit method, of class BankAccount.
-	 */
-	@Test
-	public final void testDeposit() {
-		System.out.println("deposit");
-		double amount = 100.0;
-		BankAccount instance = new BankAccount();
-		instance.deposit(amount);
-		double result = instance.getBalance();
-		assertEquals(amount, result, 0.001);
-	}
+    /**
+     * Test of deposit method, of class BankAccount.
+     */
+    @Test
+    public final void testDeposit() {
+        System.out.println("deposit");
+        double amount = 100.0;
+        BankAccount instance = new BankAccount();
+        instance.deposit(amount);
+        double result = instance.getBalance();
+        assertEquals(amount, result, 0.001);
+    }
 
-	/**
-	 * Test of withdraw method, of class BankAccount.
-	 */
-	@Test
-	public final void testWithdraw() {
-		System.out.println("withdraw");
-		double amount = 1000;
-		BankAccount instance = new BankAccount();
-		instance.deposit(amount);
-		instance.withdraw(100);
-		double result = instance.getBalance();
-		assertEquals(result, 899, 0.001);
-	}
+    /**
+     * Test of withdraw method, of class BankAccount.
+     */
+    @Test
+    public final void testWithdraw() {
+        System.out.println("withdraw");
+        double amount = 1000;
+        BankAccount instance = new BankAccount();
+        instance.deposit(amount);
+        instance.withdraw(100);
+        double result = instance.getBalance();
+        assertEquals(result, 899, 0.001);
+    }
 
-	/**
-	 * Test of getBalance method, of class BankAccount.
-	 */
-	@Test
-	public final void testGetBalance() {
-		System.out.println("getBalance");
-		BankAccount instance = new BankAccount();
-		double amount = 1000;
-		instance.deposit(amount);
-		double result = instance.getBalance();
-		assertEquals(amount, result, 0.0);
-	}
+    /**
+     * Test of getBalance method, of class BankAccount.
+     */
+    @Test
+    public final void testGetBalance() {
+        System.out.println("getBalance");
+        BankAccount instance = new BankAccount();
+        double amount = 1000;
+        instance.deposit(amount);
+        double result = instance.getBalance();
+        assertEquals(amount, result, 0.0);
+    }
 
-	/**
-	 * Test of setBalance method, of class BankAccount.
-	 */
-	@Test
-	public final void testSetBalance() {
-		System.out.println("getBalance");
-		BankAccount instance = new BankAccount(100);
-		instance.setBalance(200);
-		double result = instance.getBalance();
-		assertEquals(200, result, 0.0);
-	}
+    /**
+     * Test of setBalance method, of class BankAccount.
+     */
+    @Test
+    public final void testSetBalance() {
+        System.out.println("getBalance");
+        BankAccount instance = new BankAccount(100);
+        instance.setBalance(200);
+        double result = instance.getBalance();
+        assertEquals(200, result, 0.0);
+    }
 
 }
