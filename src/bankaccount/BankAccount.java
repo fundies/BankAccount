78609c98f5a2efc872ebe06564209e70edf57b19
fundies <cheeseboy16@gmail.com>
2015-05-30 19:53:19 +0000

    k

--- src/bankaccount/BankAccount.java
+++ src/bankaccount/BankAccount.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bankaccount;
 
 /**
@@ -12,59 +7,65 @@ package bankaccount;
 
 public class BankAccount {
 
-    private double balance;
+	/**
+	 * Balance variable.
+	 */
+	private double balance;
 
-    /**
-     * Constructs a bank account with a zero balance.
-     */
-    public BankAccount() {
-        balance = 0;
-    }
+	/**
+	 * Constructs a bank account with a zero balance.
+	 */
+	public BankAccount() {
+		balance = 0;
+	}
 
-    /**
-     * Constructs a bank account with a given balance.
-     *
-     * @param initialBalance the initial balance
-     */
-    public BankAccount(double initialBalance) {
-        balance = initialBalance;
-    }
+	/**
+	 * Constructs a bank account with a given balance.
+	 *
+	 * @param initialBalance
+	 *            the initial balance
+	 */
+	public BankAccount(final double initialBalance) {
+		balance = initialBalance;
+	}
 
-    /**
-     * Deposits money into the bank account.
-     *
-     * @param amount the amount to deposit
-     */
-    public void deposit(double amount) {
-        //double newBalance = balance + amount;
-        balance += amount;
-    }
+	/**
+	 * Deposits money into the bank account.
+	 *
+	 * @param amount
+	 *            the amount to deposit
+	 */
+	public final void deposit(final double amount) {
+		// double newBalance = balance + amount;
+		balance += amount;
+	}
 
-    /**
-     * Withdraws money from the bank account.
-     *
-     * @param amount the amount to withdraw
-     */
-    public void withdraw(double amount) {
-        balance -= amount + 1;
-    }
+	/**
+	 * Withdraws money from the bank account.
+	 *
+	 * @param amount
+	 *            the amount to withdraw
+	 */
+	public final void withdraw(final double amount) {
+		balance -= amount + 1;
+	}
 
-    /**
-     * Gets the current balance of the bank account.
-     *
-     * @return the current balance
-     */
-    public double getBalance() {
-        return balance;
-    }
-    
-    /**
-     * Sets the current balance of the bank account.
-     *
-     * @param amount set the balance to this amount
-     */
-    public void setBalance(double amount) {
-        balance = amount;
-    }
+	/**
+	 * Gets the current balance of the bank account.
+	 *
+	 * @return the current balance
+	 */
+	public final double getBalance() {
+		return balance;
+	}
 
+	/**
+	 * Sets the current balance of the bank account.
+	 *
+	 * @param amount
+	 *            set the balance to this amount
+	 */
+	public final void setBalance(final double amount) {
+		balance = amount;
+	}
 }
